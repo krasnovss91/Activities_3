6fe1e4804923f11d1b8868073834cd71d6df120f
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-09 10:59:28 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication113/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication113
+
+import android.content.Intent
+import android.os.Bundle
+
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var tvName: TextView? = null
+    var btnName: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvName = findViewById<View>(R.id.textView) as TextView
+        btnName = findViewById<View>(R.id.button) as Button
+        btnName!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val intent = Intent(this, NameActivity::class.java)
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data == null) {
+            return
+        }
+        val name = data.getStringExtra("name")
+        tvName!!.text = "Your name is $name"
+    }
+}
---
+++ app/src/main/java/com/example/myapplication113/NameActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication113
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
+class NameActivity : AppCompatActivity(), View.OnClickListener {
+    var editText: EditText? = null
+    var button: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name)
+        editText = findViewById<View>(R.id.editTextTextPersonName) as EditText
+        button = findViewById<View>(R.id.button2) as Button
+        button!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val intent = Intent()
+        intent.putExtra("name", editText!!.text.toString())
+        setResult(RESULT_OK, intent)
+        finish()
+    }
+}
