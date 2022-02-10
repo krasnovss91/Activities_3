2684c08379757c804db2314b44d10e80e29d9135
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-10 15:26:48 +0400

    Parcelable_1

--- app/src/main/java/com/example/myapplication113/MainActivity.kt
+++ app/src/main/java/com/example/myapplication113/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
 const val KEY_NAME = "KEY_NAME"
+const val USER = "user"
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     var tvName: TextView? = null
@@ -27,7 +28,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val firstName = findViewById<EditText>(R.id.editTextTextPersonName2)
         val lastName = findViewById<EditText>(R.id.editTextTextPersonName3)
 
-        val user = User(firstName,lastName,)//соберём экземпляр пользователя - имя, фамилия и информация о работе
+        val user = User(firstName.toString(), lastName, tvName)//соберём экземпляр пользователя - имя, фамилия и информация о работе
 
         saveButton.setOnClickListener(object : View.OnClickListener {//собрать экземпляр класса-наследника Parcelable и сохранить его здесь
             override fun onClick(v: View?) {
--- app/src/main/java/com/example/myapplication113/User.kt
+++ app/src/main/java/com/example/myapplication113/User.kt
@@ -1,34 +1,7 @@
 package com.example.myapplication113
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-
-data class User(val firstName: String?, val lastName: String?, val workInfo: String?):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(firstName)
-        parcel.writeString(lastName)
-        parcel.writeString(workInfo)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User {
-            return User(parcel)
-        }
-
-        override fun newArray(size: Int): Array<User?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class User(val firstName: String, val lastName: String, val workInfo: String):Parcelable
